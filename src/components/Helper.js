584355bb7522ad0504394ae60970b37019233e92
Joel Piña <joelpina93@gmail.com>
2021-01-30 13:02:00 -0300

    first commit

---
+++ src/components/Helper.js
@@ -0,0 +1,25 @@
+export function obtenerDiferenciaYear(year) {
+    return new Date().getFullYear() - year;
+}
+
+export function calcularMarca(marca) {
+    let incremento;
+    switch (marca) {
+        case 'americano':
+            incremento = 1.15;
+            break;
+        case 'europeo':
+            incremento = 1.3;
+            break;
+        case 'asiatico':
+            incremento = 1.05;
+            break;
+        default:
+            break;
+    }
+    return incremento;
+}
+
+export function calcularPlan(plan) {
+    return plan === 'basico' ? 1.2 : 1.5;
+}
